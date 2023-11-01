31fc940ba5f4ef370a933a08ef1fc4b5c11c7f1e
sdcs23 <apolocubes@gmail.com>
2023-11-01 18:12:34 -0500

    session 1/11

---
+++ Curso JS/complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,30 @@
+'use strict'; // Activates strict mode
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive :D`);
+
+// const interface = `Audio`;
+// const private = 534;
+*////////////////////////////////////////
+// Functions
+
+function logger(){
+  console.log(`My name is Simon`);
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
